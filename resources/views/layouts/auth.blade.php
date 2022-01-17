dfa22ccde63d92183df48cc45e814994dcd41966
yusuke804 <14au064f@al.rikkyo.ac.jp>
2022-01-17 21:54:32 +0900

    テンプレートファイルの作成及びメモ作成画面の作成

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -75,30 +75,8 @@
             </div>
         </nav>
 
-
-
-        <main class="">
-          <div class="row">
-						<div class="col-md-2 p-0">
-							<div class="card">
-								<div class="card-header">左カラム</div>
-								<div class="card-body">
-									<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-4 p-0">
-							<div class="card">
-								<div class="card-header">中央カラム</div>
-								<div class="card-body">
-									<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-								</div>
-							</div>
-							</div>
-						<div class="col-md-6 p-0">
-							@yield('content')
-						</div>
-          </div>
+        <main class="py-4">
+            @yield('content')
         </main>
     </div>
 </body>

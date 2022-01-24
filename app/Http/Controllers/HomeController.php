8b6bcbb3186bd32ac521027347314b9e0e61f33d
yusuke804 <14au064f@al.rikkyo.ac.jp>
2022-01-24 21:21:02 +0900

    タグ機能、新規タグの追加

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Memo;
+use App\models\Tag;
+use App\models\MemoTag;
+use DB;
 
 
 class HomeController extends Controller
@@ -31,18 +34,44 @@ class HomeController extends Controller
             ->orderBy('updated_at', 'DESC') //ASC=昇順、 DESC=降順
             ->get();
 
+        $tags = Tag::where('user_id', '=', \Auth::id())
+        ->whereNull('deleted_at')
+        ->orderBy('id', 'DESC')
+        ->get();
 
-        return view('create', compact('memos'));
+
+
+
+        return view('create', compact('memos', 'tags'));
     }
 
 
     public function store(Request $request)
     {
         $posts = $request->all();
+
         //dd dump dieの略　メソッドの引数に取った値を展開して止める　→　データの確認をするためのデバッグ関数
 
-        Memo::insert(['content' => $posts['content'], 'user_id' => \Auth::id()]);
-
+        //-----ここからトランザクションを開始-----
+        DB::transaction(function() use($posts) {
+            //メモをインサート、さらに、memo_tagテーブル(メモとタグの中間テーブル)にインサートするための、メモのidを取得、memo_id変数に保存
+            $memo_id = Memo::insertGetId(['content' => $posts['content'], 'user_id' => \Auth::id()]);
+            //すでに作成するタグが存在するかをチェックするための変数を用意する
+            $tag_exists = Tag::where('user_id', '=', \Auth::id())->where('name', '=', $posts['new_tag'])
+            ->exists();
+            //新しいタグが入力されているか、すでに作成するタグが存在しているかをチェック
+            if(!empty($posts['new_tag']) && !$tag_exists ) {
+                //タグテーブルに新規タグを挿入すると同時に、memo_tagsテーブルに保存するためのtag_idも同時に変数に保管
+                $tag_id = Tag::insertGetId(['user_id' => \Auth::id(), 'name' => $posts['new_tag']]);
+                //memo_tagsテーブルに挿入
+                MemoTag::insert(['memo_id' => $memo_id, 'tag_id' => $tag_id]);
+            }
+            //既存タグが紐づけられた場合→memo_tagsに挿入する
+            foreach($posts['tags'] as $tag){
+                MemoTag::insert(['memo_id' => $memo_id, 'tag_id' => $tag]);
+            }
+        });
+        //-----ここまで
         return redirect( route('home') );
     }
 
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,9 +5,16 @@
 	<div class="card-header">新規メモ作成</div>
 	<form class="card-body" action="{{ route('store') }}" method="POST">
         @csrf
-        <div class="form-group">
+        <div class="form-group mb-3">
             <textarea class="form-control" name="content" rows="3" placeholder="ここにメモを入力"></textarea>
         </div>
+    @foreach ($tags as $t)
+        <div class="form-check form-check-inline mb-3">
+            <input class="form-check-input" type="checkbox" name="tags[]" id="{{ $t['id'] }}" value="{{ $t['id'] }}" >
+            <label class="form-check-label" for="{{ $t['id'] }}">{{ $t['name'] }}</label>
+        </div>
+    @endforeach
+        <input type="text" class="form-control w-50 mb-3" name="new_tag" placeholder="新しいタグを入力">
         <button type="submit" class="btn btn-primary">保存</button>
 	</form>
 </div>

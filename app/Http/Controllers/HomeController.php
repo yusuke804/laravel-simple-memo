f76860075764986c3b55da80e7119adba49dbbfc
yusuke804 <14au064f@al.rikkyo.ac.jp>
2022-02-01 15:40:57 +0900

    view composerの導入、メモ一覧表示の共通化

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,21 +28,12 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $memos = Memo::select('memos.*')
-            ->where('user_id', '=', \Auth::id())
-            ->whereNull('deleted_at')
-            ->orderBy('updated_at', 'DESC') //ASC=昇順、 DESC=降順
-            ->get();
-
         $tags = Tag::where('user_id', '=', \Auth::id())
         ->whereNull('deleted_at')
         ->orderBy('id', 'DESC')
         ->get();
 
-
-
-
-        return view('create', compact('memos', 'tags'));
+        return view('create', compact('tags'));
     }
 
 
@@ -79,12 +70,6 @@ class HomeController extends Controller
 
     public function edit($id)
     {
-        $memos = Memo::select('memos.*')
-            ->where('user_id', '=', \Auth::id())
-            ->whereNull('deleted_at')
-            ->orderBy('updated_at', 'DESC') //ASC=昇順、 DESC=降順
-            ->get();
-
         $edit_memo = Memo::select('memos.*', 'tags.id AS tag_id')
             ->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
             ->leftJoin('tags', 'memo_tags.tag_id', '=', 'tags.id')
@@ -103,7 +88,7 @@ class HomeController extends Controller
         ->orderBy('id', 'DESC')
         ->get();
 
-        return view('edit', compact('memos', 'edit_memo', 'include_tags', 'tags'));
+        return view('edit', compact('edit_memo', 'include_tags', 'tags'));
     }
 
     public function update(Request $request)
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Memo;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 全てのメソッドが呼ばれる前に先に呼ばれるメソッド
+        // 今回は、メモ一覧を表示するにあたって、一覧画面と編集画面で重複している処理をひとまとめ（共通化）する
+        view()->composer('*', function ($view) {
+            $memos = Memo::select('memos.*')
+            ->where('user_id', '=', \Auth::id())
+            ->whereNull('deleted_at')
+            ->orderBy('updated_at', 'DESC') //ASC=昇順、 DESC=降順
+            ->get();
+
+        $view->with('memos', $memos);
+        });
     }
 }
